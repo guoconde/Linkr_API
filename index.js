1efd8c5163a8188cb59940f7390ccff8ee18bcfd
Gustavo Conde <guoconde@pop-os.localdomain>
2022-03-22 09:57:35 -0300

    Feat: Structuring folders

---
+++ index.js
@@ -0,0 +1,11 @@
+import express, { json } from "express"
+import cors from "cors"
+import router from "./routes/index.js"
+
+const app = express()
+
+app.use(cors())
+app.use(json())
+app.use(router)
+
+app.listen(4000, () => { console.log('Server listening on PORT 4000')})
