04f4629305695c235f0d06c97853c7efbed55de1
Rui Neto <ruineto11@gmail.com>
2022-03-28 01:19:00 -0300

    feat: getUserById in the route GET /users/:id

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,7 +13,7 @@ export async function register(req, res) {
       FROM users
       WHERE email=$1
     `, [email]);
-    if(searchedUser.rowCount !== 0){
+    if (searchedUser.rowCount !== 0) {
       res.sendStatus(409);
       return;
     }
@@ -41,12 +41,37 @@ export async function findUsers(req, res) {
     if (!find) {
       return res.send([]);
     }
-    
+
     const data = await findUsersInput([find])
-    
+
     res.send(data);
-} catch (error) {
+  } catch (error) {
     console.log(error);
     res.status(500).send("Unexpected server error")
+  }
 }
+
+export async function getUserById(req, res) {
+  const { id } = req.params;
+  if(!id){
+    res.send(400).send("To get the user, it is necessary to pass through query params the id of the same!");
+    return;
+  }
+
+  try {
+    const searchedUser = await connection.query(`
+      SELECT
+        u.photo
+      FROM users AS u
+      WHERE id=$1
+    `, [id]);
+    if(!searchedUser.rowCount){
+      res.send(404).send("Sorry, user not found!");
+    }
+    
+    res.send(searchedUser.rows[0].photo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Unexpected server error");
+  }
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { findUsers, register } from "../controllers/userController.js";
+import { findUsers, getUserById, register } from "../controllers/userController.js";
 import { listPosts } from "../controllers/postsController.js";
 import validateSchemaMiddleware from "../middlewares/validateSchemaMiddleware.js";
 import validateTokenMiddleware from "../middlewares/validateTokenMiddleware.js";
 
 const userRouter = Router();
 userRouter.post('/sign-up', validateSchemaMiddleware, register);
-userRouter.get('/users', findUsers)
+userRouter.get('/users', findUsers);
+userRouter.get('/users/:id', getUserById);
 userRouter.get('/user/:id', validateTokenMiddleware, listPosts);
 export default userRouter;
