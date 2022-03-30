ebaadcce6e5383a6bee71094ad6a4d9357d72d63
Rui Neto <ruineto11@gmail.com>
2022-03-30 04:12:04 -0300

    feat: list comments on the route GET /comments/:postId

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -2,7 +2,7 @@ import connection from "../db.js";
 
 export async function createComment(req, res) {
   const { userId, postId, comment } = req.body;
-  
+
   try {
     await connection.query(`
       INSERT INTO comments
@@ -10,8 +10,33 @@ export async function createComment(req, res) {
       VALUES
         ($1, $2, $3)
     `, [userId, postId, comment]);
-    
+
     res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Unexpected server error");
+  }
+}
+
+export async function listComments(req, res) {
+  const { postId } = req.params;
+  if (!postId) {
+    res.status(404).send("Post not found!");
+  }
+
+  try {
+    const comments = await connection.query(`
+      SELECT 
+        c.id,
+        u.name, u.photo,
+        c.comment
+      FROM comments AS c
+        JOIN users AS u ON u.id=c."userId"
+      WHERE c."postId"=$1
+      ORDER BY c.id
+    `, [postId]);
+
+    res.send(comments.rows);
   } catch (error) {
     console.log(error);
     res.status(500).send("Unexpected server error");
